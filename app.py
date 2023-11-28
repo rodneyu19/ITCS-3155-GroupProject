427447064a6effd65c750638505264dae9f7fbf1
cfreeman02 <johnconnorfreeman1@gmail.com>
2023-11-28 17:22:07 -0500

    this is to create posts utilizing the spotify api which will pull users and be able to make posts with spotify links

--- app.py
+++ app.py
@@ -1,35 +1,54 @@
-from flask import Flask, render_template, request, redirect
-from src.models import db, Post
-from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
+from src.models import db, Post, Comment
 
 app = Flask(__name__)
 
-load_dotenv()
-
-if None in (os.getenv("DB_USER"), os.getenv("DB_PASS"), os.getenv("DB_HOST"), os.getenv("DB_PORT"), os.getenv("DB_NAME")):
-    raise ValueError("Fix the variables in your .env lmao")
-
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
-
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Replace with your database URI
 db.init_app(app)
 
-@app.get('/')
+# Spotify credentials setup
+client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"))
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Routes
+@app.route('/')
 def index():
     all_posts = Post.query.all()
     return render_template('home.html', all_posts=all_posts)
 
-@app.get('/post/new')
-def create_post_form():
-    return render_template('create_post.html')
+@app.route('/post/<spotify_id>')
+def view_post(spotify_id):
+    post = Post.query.filter_by(spotify_id=spotify_id).first()
 
-@app.post('/post/new')
-def create_post():
-    title = request.form.get('title')
-    body = request.form.get('body')
-    new_post = Post(title=title, body=body)
-    db.session.add(new_post)
+    if post is None:
+        print(f"No post found for spotify_id: {spotify_id}")
+        return "Post not found"
+
+    comments = Comment.query.filter_by(post_id=post.id).all()
+    return render_template('view_post.html', post=post, comments=comments)
+
+@app.route('/post/<spotify_id>/comment', methods=['POST'])
+def add_comment(spotify_id):
+    post = Post.query.filter_by(spotify_id=spotify_id).first()
+
+    if post is None:
+        print(f"No post found for spotify_id: {spotify_id}")
+        return "Post not found"
+
+    comment_text = request.form.get('comment')
+    new_comment = Comment(comment_text=comment_text, post_id=post.id)
+    db.session.add(new_comment)
     db.session.commit()
-    return redirect('/')
+    return redirect(url_for('view_post', spotify_id=spotify_id))
 
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
--- src/models.py
+++ src/models.py
@@ -1,8 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Post(db.Model):
-    post_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(255), nullable=False)
-    body = db.Column(db.Text, nullable=False)
+    __tablename__ = 'posts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    spotify_id = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+# Inside src/models.py
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment_text = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    def __init__(self, comment_text, post_id):
+        self.comment_text = comment_text
+        self.post_id = post_id
+
