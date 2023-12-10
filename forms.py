a0981428350c8827fc67f24bb8177a6ff42b2685
lixjia <JiaLi@Jias-MacBook-Pro.local>
2023-12-10 01:53:43 -0500

    added functional api and css changes

--- forms.py
+++ forms.py
@@ -4,14 +4,13 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired(), Length(min=2, max =20)])
-	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password', 
 									 validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 	
 class LoginForm(FlaskForm):
-	email = StringField('Email', validators=[DataRequired(), Email()])
+	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
