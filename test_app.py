c6ed644606049105d45657333045f60eb8a7dc08
cfreeman02 <116218225+cfreeman02@users.noreply.github.com>
2023-12-13 21:39:27 -0500

    Update test_app.py
    Updated pytest

--- test_app.py
+++ test_app.py
@@ -1,187 +1,52 @@
-import unittest
-from flask import Flask, url_for
-from src.models import db, Post, Users
-from flask import Flask
-from flask_login import current_user
-from app import  db
-from src.models import Users
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import pytest
+from app import app as flask_app
 
-def create_app(testing=True):
-    app = Flask(__name__)
-    loginManager = LoginManager(app)
-    app.config['TESTING'] = testing
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+@pytest.fixture
+def app():
+    yield flask_app
 
-    # Initialize the test database
-    with app.app_context():
-        db.init_app(app)
-        db.create_all()
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
-    return app
+def test_home_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Welcome to the homepage' in response.data 
 
-class AppTesting(unittest.TestCase):
-    def setUp(self):
-        # Create a test app
-        self.app = create_app(testing=True)
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+def test_registration_route(client):
+    response = client.get('/register')
+    assert response.status_code == 200
+    assert b'Register' in response.data  
 
-        # Create a test client
-        self.client = self.app.test_client()
+def test_login_route(client):
+    response = client.get('/login')
+    assert response.status_code == 200
+    assert b'Login' in response.data  
 
-        # Create the test database schema
-        with self.app.app_context():
-            db.create_all()
-    
-    def tearDown(self):
-        # Remove the test database
-        with self.app.app_context():
-            db.session.remove()
-            db.drop_all()
+def test_logout_route(client):
+    response = client.get('/logout')
+    assert response.status_code == 302 
 
-        self.app_context.pop()
+def test_post_creation(client):
+    with client:
+        client.post('/login', data=dict(username='test_user', password='test_password'))
 
-    # Test the index route
-    def test_index_route(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Welcome to the Home Page', response.data)
-        
-    def test_register_user(self):
-        response = self.client.post('/register', data=dict(
-            username='testuser',
-            password='testpassword',
-            confirm_password='testpassword'
+        response = client.post('/post/new', data=dict(
+            title='Test Post Title',
+            body='Test Post Body',
+            link='https://example.com'
         ), follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-        user = Users.query.filter_by(username='testuser').first()
-        self.assertIsNotNone(user)
-    def test_login_user(self):
-        # Assuming you have registered a test user before
-        test_user = Users(username='testuser', password='testpassword')
-        db.session.add(test_user)
-        db.session.commit()
 
-        response = self.client.post('/login', data=dict(
-            username='testuser',
-            password='testpassword'
-        ), follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(current_user.is_authenticated)
-    def test_login_with_wrong_credentials(self):
-        response = self.client.post('/login', data=dict(
-            username='wronguser',
-            password='wrongpassword'
-        ), follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(current_user.is_authenticated)
-        
-    def test_edit_profile(self):
-        #Assuming you have a logged-in user
-        test_user = Users(username='testuser', password='testpassword')
-        db.session.add(test_user)
-        db.session.commit()
-        login_user(test_user, remember=True ,force=True)
-        response = self.client.get('/profile/edit')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200 
+        assert b'Test Post Title' in response.data 
 
-            # Perform an edit profile action (replace with your actual form data)
-            # response = self.client.post('/profile/edit', data=dict(
-			username='newusername',
-			firstname='New',
-			lastname='User',
-			password='newpassword',
-			confirm_password='newpassword',
-			save=True
-		), follow_redirects=True)
+def test_profile_route(client):
+    response = client.get('/profile')
+    assert response.status_code == 200
+    assert b'Profile' in response.data 
 
-            # Assert that the edit was successful or check for expected behavior
-            self.assertEqual(response.status_code, 200)
-            # Add more assertions based on your application's behavior
-
-            # Log out the user after testing (optional)
-            logout_user()
-        
-
-        # with self.client:
-            
-        #     self.client.post('/login', data=dict(
-        #         username='testuser',
-        #         password='testpassword'
-        #     ), follow_redirects=True)
-            
-        #     response = self.client.get('/profile.html')
-        #     self.assertEqual(response.status_code, 200)
-
-        #     response = self.client.post('/profile', data=dict(
-        #         username='newusername',
-        #         firstname='New',
-        #         lastname='User',
-        #         password='newpassword',
-        #         confirm_password='newpassword'
-        #     ), follow_redirects=True)
-        #     db.session.commit()
-
-        #     self.assertEqual(response.status_code, 200)
-        #     user = Users.query.filter_by(username='newusername').first()
-        #     self.assertIsNotNone(user)
-        #     self.assertEqual(user.firstname, 'New')
-        #     self.assertEqual(user.lastname, 'User')
-            
-            
-
-            # Access the edit profile page
-            
-            
-	
-
-	# # Test home route
-    # def test_home_route(self):
-    #     response = self.client.get('/home')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Welcome to the Home Page', response.data)
-
-    # # Test create post route
-    # def test_create_post_route(self):
-    #     response = self.client.get('/post/new')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Create Post', response.data)
-
-    #     response = self.client.post('/post/new', data={'title': 'Test Title', 'body': 'Test Body', 'link': 'Test Link'})
-    #     self.assertEqual(response.status_code, 302)
-
-    # # Test logout()
-    # def test_logout_route(self):
-    #     response = self.client.get(url_for('logout'))
-    #     self.assertEqual(response.status_code, 302)
-
-    # # Test spotifylogin()
-    # def test_spotifylogin_route(self):
-    #     response = self.client.get(url_for('spotifylogin'))
-    #     self.assertEqual(response.status_code, 302)
-	#     # Test delete_post()
-    # def test_delete_post_route(self):
-    #     # have a post with ID=1 for testing
-    #     response = self.client.get(url_for('delete_post', post_id=1))
-    #     self.assertEqual(response.status_code, 302)
-    # Test search()
-    # def test_search_route(self):
-    #     response = self.client.post(url_for('search'), data={'searched': 'your_search_term'})
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Search Results', response.data)
-    # Test register()
-    # def test_register_route(self):
-    #     response = self.client.get('/register')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Register', response.data)
-
-    # # Test the login()
-    # def test_login_route(self):
-    #     response = self.client.get('/login')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Login', response.data)
-
-if __name__ == '__main__':
-    unittest.main()
-    
+def test_search_route(client):
+    response = client.post('/search', data=dict(searched='test_query'))
+    assert response.status_code == 200
+    assert b'Search Results' in response.data  
